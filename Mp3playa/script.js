05eede52d80fadf2b270afaf5e1f72185bf3fa7a
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-09 14:22:31 +0530

    Add files via upload

--- Mp3playa/script.js
+++ Mp3playa/script.js
@@ -2,21 +2,45 @@
 
 let songIndex = 0;
 
-let audioElement = new Audio('FOD.mp3')
+let audioElement = new Audio('song-list/FOD.mp3')
 
 // audioElement.play()
 
 let masterPlay = document.getElementById("masterPlay")
 
+let myProgressBar = document.getElementById("myProgressBar")
+
 
 let songs = [
-    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
-    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
-    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
-    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
-    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"}
-
+    {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"}
 ]
+
+//handle play/pause button
+
+masterPlay.addEventListener('click', ()=>{
+
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play()
+
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
+
+    else{
+        audioElement.pause()
+
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        
+    }
+})
 
 //Listen to events
 
+myProgressBar.addEventListener("timeupdate", ()=>{
+
+})
