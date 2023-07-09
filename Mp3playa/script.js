b247eefcd38216de4b208c9fe4583c1a2b3c0c0d
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-09 14:00:09 +0530

    Add files via upload

---
+++ Mp3playa/script.js
@@ -0,0 +1,12 @@
+let audioElement = new Audio('FOD.mp3')
+
+audioElement.play()
+
+let songs = [
+    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"},
+    {songName : "Fear Of the Dark", songPath : "Fod.mp3", songCover : "cover1.jpg"}
+
+]
