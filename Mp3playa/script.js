53d52d298253e762c2ff2c60d1f139e3c3e573f1
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-07-09 14:35:37 +0530

    Add files via upload

--- Mp3playa/script.js
+++ Mp3playa/script.js
@@ -10,6 +10,8 @@ let masterPlay = document.getElementById("masterPlay")
 
 let myProgressBar = document.getElementById("myProgressBar")
 
+let gif = document.getElementById("gif")
+
 
 let songs = [
     {songName : "Fear Of the Dark", songPath : "FOD.mp3", songCover : "cover1.jpg"},
@@ -28,6 +30,7 @@ masterPlay.addEventListener('click', ()=>{
 
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
     }
 
     else{
@@ -35,12 +38,18 @@ masterPlay.addEventListener('click', ()=>{
 
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');
-        
+        gif.style.opacity = 0;
     }
 })
 
 //Listen to events
 
-myProgressBar.addEventListener("timeupdate", ()=>{
+audioElement.addEventListener("timeupdate", ()=>{
+
+    //logic to calculate progress% and update the progress bar
+   var progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+
+    myProgressBar.value = progress;
+
 
 })
